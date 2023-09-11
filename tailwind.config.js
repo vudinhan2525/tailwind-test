3e0da1cc3aa9baf0c33ab7c9b436a1d262ca5a76
vudinhan2525 <vudinhan000@gmail.com>
2023-09-11 11:00:08 +0700

    t

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./public/**/*.{html,js}'],
+    theme: {
+        extend: {
+            colors: {
+                'primary-color': '#C89F94',
+            },
+        },
+    },
+    plugins: [],
 };
