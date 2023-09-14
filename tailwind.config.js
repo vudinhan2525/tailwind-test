493caf649c28b3dbeb4c40bda0f028bd2b90f8fa
vudinhan2525 <vudinhan000@gmail.com>
2023-09-14 17:38:09 +0700

    nt

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,9 +15,14 @@ module.exports = {
                 '0%': { transform: 'translateY(-90%)' },
                 '90%': { transform: 'translateY(0)' },
             },
+            fadeIn: {
+                from: { opacity: 0 },
+                to: { opacity: 1 },
+            },
         },
         animation: {
             slideDown: 'slideDown 1s ease-in-out',
+            fadeIn: 'fadeIn .5s ease-in-out',
         },
     },
     plugins: [],
